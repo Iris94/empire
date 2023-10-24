19325b937cc274c6c5d31dcbf59cab1539f4d26c
Iris94 <mirza.iris94@gmail.com>
2023-10-24 15:00:06 +0200

    testing further

--- src/app/components/batlegroundComp/player/Player.tsx
+++ src/app/components/batlegroundComp/player/Player.tsx
@@ -9,16 +9,14 @@ const Player = () => {
         <div className='w-full h-full z-20'>
 
         </div>
-        <div className='w-full lg:h-1/3 h-1/5 fractal z-20 flex justify-center items-center p-2'>
-          <div className='w-2/6 h-full fractal'></div>
-          <div className='w-2/6 flex items-end justify-center h-full fractal'>
-            <div className='lg:w-2/3 w-full flex z-30 justify-between items-end playerHud blue-card'>
+
+        <div className='w-full lg:h-1/3 fractal paper-card 
+        z-20 flex justify-center items-end p-2'>
+            <div className='w-fit h-fit flex z-30 justify-between items-end playerHud dark-blue-card'>
               <HealthHUD />
               <PlayerHUD />
               <ManaHUD />
             </div>
-          </div>
-          <div className='w-2/6 h-full fractal'></div>
         </div>
     </div>
   )
