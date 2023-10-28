aeb6c26fe8c1802e7b04ab89a3ee02e6dc460e2e
Iris94 <mirza.iris94@gmail.com>
2023-10-28 18:53:49 +0200

    Test for mobile phones full screen mode

--- src/app/GlobalRedux/provider.tsx
+++ src/app/GlobalRedux/provider.tsx
@@ -9,18 +9,51 @@ import RotatePhone from "../components/sideScreens/RotatePhone";
 export function Providers ({children} : any) {
     const [isPortrait, setIsPortrait] = useState(true);
 
-    useEffect(() => {
-        function handlePhoneChange () {
-            setIsPortrait(window.innerHeight > window.innerWidth)
-        }
+    function handlePhoneChange() {
+        setIsPortrait(window.innerHeight > window.innerWidth);
+    }
 
+    function handleClick() {
+        if (isPhoneScreen()) {
+            if (document.fullscreenElement) {
+                // If the game is in fullscreen and it's a phone, exit fullscreen
+                document.exitFullscreen();
+            } else {
+                // If the game is not in fullscreen and it's a phone, request fullscreen
+                document.documentElement.requestFullscreen();
+            }
+        }
+    }
+    
+
+    // Function to check if the screen size corresponds to a phone
+    function isPhoneScreen() {
+        // Adjust these values according to your requirements
+        const minWidth = 415; // Minimum screen width for a phone
+        const maxWidth = 950; // Maximum screen width for a phone
+        const screenWidth = window.innerWidth;
+        return screenWidth >= minWidth && screenWidth <= maxWidth;
+    }
+
+    useEffect(() => {
         handlePhoneChange();
-        window.addEventListener('resize', handlePhoneChange)
+        window.addEventListener('resize', handlePhoneChange);
+
+        // Check if the Fullscreen API is supported by the browser
+        if (document.documentElement.requestFullscreen!) {
+            // Add or remove the click event listener based on orientation and screen size
+            if (!isPortrait && isPhoneScreen()) {
+                window.addEventListener('click', handleClick);
+            } else {
+                window.removeEventListener('click', handleClick);
+            }
+        }
 
         return () => {
-            window.removeEventListener('resize', handlePhoneChange)
+            window.removeEventListener('resize', handlePhoneChange);
+            window.removeEventListener('click', handleClick); // Remove the click event listener on cleanup
         }
-    }, [])
+    }, [isPortrait]);
 
     return (
         <Provider store={store}>
